4c149b0a64f722972d093799ccaf63a50853fa1f
Petre Boboc <petre@boboc.info>
2019-02-07 17:01:14 +0200

    Final release

--- piano.py
+++ piano.py
@@ -9,15 +9,31 @@ while True:
     var = ser.readline()
     var = float(var)
 
-    if var==1:
-        winsound.PlaySound("res\piano\A0.wav", winsound.SND_ALIAS)
-    if var==2:
-        winsound.PlaySound("res\piano\A1.wav", winsound.SND_ALIAS)
-    if var==3:
-        winsound.PlaySound("res\piano\A2.wav", winsound.SND_ALIAS)
-    if var==4:
-        winsound.PlaySound("res\piano\A3.wav", winsound.SND_ALIAS)
-    if var==5:
-        winsound.PlaySound("res\piano\A4.wav", winsound.SND_ALIAS)
-    if var==6:
-        winsound.PlaySound("res\piano\A5.wav", winsound.SND_ALIAS)
+    instrument = 1
+
+    if instrument==0:
+        if var==1:
+            winsound.PlaySound("res\piano\A0.wav", winsound.SND_ALIAS)
+        if var==2:
+            winsound.PlaySound("res\piano\A1.wav", winsound.SND_ALIAS)
+        if var==3:
+            winsound.PlaySound("res\piano\A2.wav", winsound.SND_ALIAS)
+        if var==4:
+            winsound.PlaySound("res\piano\A3.wav", winsound.SND_ALIAS)
+        if var==5:
+            winsound.PlaySound("res\piano\A4.wav", winsound.SND_ALIAS)
+        if var==6:
+            winsound.PlaySound("res\piano\A5.wav", winsound.SND_ALIAS)
+    else:
+        if var==1:
+            winsound.PlaySound("res\drums\A0.wav", winsound.SND_ALIAS)
+        if var==2:
+            winsound.PlaySound("res\drums\A1.wav", winsound.SND_ALIAS)
+        if var==3:
+            winsound.PlaySound("res\drums\A2.wav", winsound.SND_ALIAS)
+        if var==4:
+            winsound.PlaySound("res\drums\A3.wav", winsound.SND_ALIAS)
+        if var==5:
+            winsound.PlaySound("res\drums\A4.wav", winsound.SND_ALIAS)
+        if var==6:
+            winsound.PlaySound("res\drums\A5.wav", winsound.SND_ALIAS)
--- sketch_piano/sketch_piano.ino
+++ sketch_piano/sketch_piano.ino
@@ -5,41 +5,44 @@ void setup()
 
 void loop()
 {
-  if(analogRead(0)<800)
+
+  int sensorValue[6] = {800, 800, 800, 800, 800, 800};
+
+  if (analogRead(0) < sensorValue[0])
   {
     Serial.println(1);
     delay(850);
   }
 
-  if(analogRead(1)<800)
+  if (analogRead(1) < sensorValue[1])
   {
     Serial.println(2);
     delay(950);
   }
 
-  if(analogRead(2)<800)
+  if (analogRead(2) < sensorValue[2])
   {
     Serial.println(3);
     delay(870);
   }
 
-  if(analogRead(3)<800)
+  if (analogRead(3) < sensorValue[3])
   {
     Serial.println(4);
     delay(820);
   }
 
-  if(analogRead(4)<800)
+  if (analogRead(4) < sensorValue[4])
   {
     Serial.println(5);
     delay(830);
   }
 
-  if(analogRead(5)<800)
+  if (analogRead(5) < sensorValue[5])
   {
     Serial.println(6);
     delay(880);
   }
-  
+
 }
 
