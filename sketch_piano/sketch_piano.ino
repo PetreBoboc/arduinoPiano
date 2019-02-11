a3d6c7af70baa7e7096710838a5b556d7742f153
Petre Boboc <petre@boboc.info>
2019-02-11 21:12:09 +0200

    Small fixes

--- sketch_piano/sketch_piano.ino
+++ sketch_piano/sketch_piano.ino
@@ -6,42 +6,43 @@ void setup()
 void loop()
 {
 
-  int sensorValue[6] = {800, 800, 800, 800, 800, 800};
+  unsigned int sensorValue[6] = {800, 800, 800, 800, 800, 800};
+  unsigned int sensorDelay[6] = {850, 950, 870, 820, 830, 880};
 
   if (analogRead(0) < sensorValue[0])
   {
     Serial.println(1);
-    delay(850);
+    delay(sensorDelay[0]);
   }
 
   if (analogRead(1) < sensorValue[1])
   {
     Serial.println(2);
-    delay(950);
+    delay(sensorDelay[1]);
   }
 
   if (analogRead(2) < sensorValue[2])
   {
     Serial.println(3);
-    delay(870);
+    delay(sensorDelay[2]);
   }
 
   if (analogRead(3) < sensorValue[3])
   {
     Serial.println(4);
-    delay(820);
+    delay(sensorDelay[3]);
   }
 
   if (analogRead(4) < sensorValue[4])
   {
     Serial.println(5);
-    delay(830);
+    delay(sensorDelay[4]);
   }
 
   if (analogRead(5) < sensorValue[5])
   {
     Serial.println(6);
-    delay(880);
+    delay(sensorDelay[5]);
   }
 
 }
